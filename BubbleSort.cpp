8fbfda8a82931e552988a120df7c4c0d8830d7be
Kwabena Ntiamoah Boateng <kboatb0@gmail.com>
2025-01-05 21:53:07 +0000

    Added comments

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -2,13 +2,19 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-    int numbers[] = {36, 21, 19, 73, 55, 93, 89, 1, 6, 7, 12, 20, 45};
+    // Array of numbers to sort
+    int numbers[] = { 36, 21, 19, 73, 55, 93, 89, 1, 6, 7, 12, 20, 45 };
+
+    // Number of items in the array
     int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    // A boolean to keep track of the swapped numbers
     bool swapped;
 
-    for (int i = 0; i < size; i++){
+    // Nested for-loop that iterates over the numbers and swaps them if they are disordered
+    for (int i = 0; i < size; i++) {
         swapped = false;
         for (int j = 0; j < size - 1 - i; j++) {
             if (numbers[j] > numbers[j + 1]) {
@@ -18,13 +24,15 @@ int main(){
                 swapped = true;
             }
         }
-        if (!swapped) 
+        // Break out of the loop if after an iteration, no values were swapped
+        if (!swapped)
             break;
     }
 
+    // Display the sorted numbers
     for (int i = 0; i < size; i++) {
         cout << numbers[i];
-        if (i != size - 1) 
+        if (i != size - 1)
             cout << ", ";
     }
 }
